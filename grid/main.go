ada3daa6106981cf36183f92d7d2ff94eea96d2b
gernest <geofreyernest@live.com>
2017-04-18 09:20:20 +0300

    add more code samples

--- grid/main.go
+++ grid/main.go
@@ -44,12 +44,15 @@ func gridExamples() []*examples.Example {
 		},
 		&examples.Example{
 			View: grid2(),
+			Code: grid2Text,
 		},
 		&examples.Example{
 			View: grid3(),
+			Code: grid3Text,
 		},
 		&examples.Example{
 			View: grid4(),
+			Code: grid4Text,
 		},
 	}
 }
@@ -252,6 +255,43 @@ func grid2() *grid.Grid {
 	}
 }
 
+var grid2Text = `
+func cellChild2(m ...vecty.MarkupOrComponentOrHTML) vecty.MarkupOrComponentOrHTML {
+	var v []vecty.MarkupOrComponentOrHTML
+	v = append(v, m...)
+	v = append(v, style.Height(style.Px(200)))
+	v = append(v, style.Color("white"))
+	v = append(v, vecty.Style("background-color", "#BDBDBD"))
+	v = append(v, vecty.Style("box-sizing", "border-box"))
+	v = append(v, vecty.Style("padding-leftr", string(style.Px(8))))
+	v = append(v, vecty.Style("padding-top", string(style.Px(4))))
+	return vecty.List(v)
+}
+
+func grid2() *grid.Grid {
+	return &grid.Grid{
+		Cells: []*grid.Cell{
+			{
+				Mode:     grid.Default,
+				Size:     4,
+				Children: cellChild2(vecty.Text("4")),
+			},
+			{
+				Mode:     grid.Default,
+				Size:     4,
+				Children: cellChild2(vecty.Text("4")),
+			},
+			{
+				Mode:     grid.Default,
+				Size:     4,
+				Children: cellChild2(vecty.Text("4")),
+			},
+		},
+	}
+}
+
+`
+
 func grid3() *grid.Grid {
 	return &grid.Grid{
 		Cells: []*grid.Cell{
@@ -273,6 +313,42 @@ func grid3() *grid.Grid {
 		},
 	}
 }
+
+var grid3Text = `
+func cellChild2(m ...vecty.MarkupOrComponentOrHTML) vecty.MarkupOrComponentOrHTML {
+	var v []vecty.MarkupOrComponentOrHTML
+	v = append(v, m...)
+	v = append(v, style.Height(style.Px(200)))
+	v = append(v, style.Color("white"))
+	v = append(v, vecty.Style("background-color", "#BDBDBD"))
+	v = append(v, vecty.Style("box-sizing", "border-box"))
+	v = append(v, vecty.Style("padding-leftr", string(style.Px(8))))
+	v = append(v, vecty.Style("padding-top", string(style.Px(4))))
+	return vecty.List(v)
+}
+
+func grid3() *grid.Grid {
+	return &grid.Grid{
+		Cells: []*grid.Cell{
+			{
+				Mode:     grid.Default,
+				Size:     6,
+				Children: cellChild2(vecty.Text("6")),
+			},
+			{
+				Mode:     grid.Default,
+				Size:     4,
+				Children: cellChild2(vecty.Text("4")),
+			},
+			{
+				Mode:     grid.Default,
+				Size:     2,
+				Children: cellChild2(vecty.Text("2")),
+			},
+		},
+	}
+}
+`
 
 func grid4() *grid.Grid {
 	return &grid.Grid{
@@ -298,3 +374,44 @@ func grid4() *grid.Grid {
 		},
 	}
 }
+
+var grid4Text = `
+func cellChild2(m ...vecty.MarkupOrComponentOrHTML) vecty.MarkupOrComponentOrHTML {
+	var v []vecty.MarkupOrComponentOrHTML
+	v = append(v, m...)
+	v = append(v, style.Height(style.Px(200)))
+	v = append(v, style.Color("white"))
+	v = append(v, vecty.Style("background-color", "#BDBDBD"))
+	v = append(v, vecty.Style("box-sizing", "border-box"))
+	v = append(v, vecty.Style("padding-leftr", string(style.Px(8))))
+	v = append(v, vecty.Style("padding-top", string(style.Px(4))))
+	return vecty.List(v)
+}
+
+
+func grid4() *grid.Grid {
+	return &grid.Grid{
+		Cells: []*grid.Cell{
+			{
+				Mode:       grid.Default | grid.Tablet,
+				Size:       6,
+				TabletSize: 8,
+				Children:   cellChild2(vecty.Text("6 (8 tablet)")),
+			},
+			{
+				Mode:       grid.Default | grid.Tablet,
+				Size:       4,
+				TabletSize: 6,
+				Children:   cellChild2(vecty.Text("4 (6 tablet)")),
+			},
+			{
+				Mode:      grid.Default | grid.Phone,
+				Size:      2,
+				PhoneSIze: 4,
+				Children:  cellChild2(vecty.Text("2 (4 phone)")),
+			},
+		},
+	}
+}
+
+`
